02e0a790f735df7ed276e458e6543f7628d62157
LightMe <lightme42@yandex.ru>
2021-02-04 16:04:29 +0800

    rewrite urls

--- poll_proj/urls.py
+++ poll_proj/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 
-from poll_proj.poll import views as poll_views
+from poll import views as poll_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
