a8a37c41d4c99b9876b8d6b0156603f5abbf1d3e
LightMe <lightme42@yandex.ru>
2021-02-04 17:09:25 +0800

    add dynamic updating the information on the pages

--- poll/views.py
+++ poll/views.py
@@ -1,25 +1,54 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from .forms import CreatePollForm
+from .models import Poll
 
 
 def home(request):
-    context = {}
+    poll = Poll.objects.all()
+
+    context = {
+        'polls': poll
+    }
     return render(request, 'poll/home.html', context)
 
 
-def results(request):
-    context = {}
+def results(request, poll_id):
+    poll = Poll.objects.get(pk=poll_id)
+
+    context = {
+        'poll': poll
+    }
     return render(request, 'poll/results.html', context)
 
 
-def vote(request):
-    context = {}
+def vote(request, poll_id):
+    poll = Poll.objects.get(pk=poll_id)
+    if request.method == 'POST':
+        print(request.POST['poll'])
+        selected_option = request.POST['poll']
+        if selected_option == 'option1':
+            poll.option_one_count += 1
+        elif selected_option == 'option2':
+            poll.option_two_count += 1
+        elif selected_option == 'option3':
+            poll.option_three_count += 1
+        else:
+            return HttpResponse(400, 'Invalid form option')
+
+        poll.save()
+    context = {
+        'poll': poll
+    }
     return render(request, 'poll/vote.html', context)
 
 
 def create(request):
     if request.method == 'POST':
         form = CreatePollForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
     else:
         form = CreatePollForm()
 
